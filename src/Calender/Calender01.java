01170f27e33caea95312103da61415bd8326edbd
PC16 <PC16@PC16-PC>
2018-05-29 19:34:59 +0900

    find maxDays of the month

--- src/Calender/Calender01.java
+++ src/Calender/Calender01.java
@@ -1,5 +1,7 @@
 package Calender;
 
+import java.util.Scanner;
+
 public class Calender01 {
 	public static void main(String[] args) {
 
@@ -9,5 +11,13 @@ public class Calender01 {
 	System.out.println("8   9  10 11 12 13 14");
 	System.out.println("15  16 17 18 19 20 21");
 	System.out.println("22  23 24 25 26 27 28");
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("달을 입력하세요.");
+	int month = sc.nextInt();
+	int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	System.out.println(month+"월은"+ maxDays[month-1]+"일까지 있습니다");
+	sc.close();
 	}
 }
